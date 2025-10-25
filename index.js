24a6a338500ba77b4630bcde66239dfcc1608f83
nika-post <nika-post@yandex.ru>
2025-10-25 17:30:56 +0300

    Add cors package

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import routerApi from './routes/index.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+import db from "./config/db.js";
 
 const app = express();
-dotenv.config();
+app.use(cors());
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,23 +14,16 @@ const __dirname = path.dirname(__filename);
 app.use('/', express.static('./public'));
 
 app.get("/", (request, response) => {
-  // response.send("<h1>API LINK NOTE</h1>");
   response.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-const { PORT, URI_DB } = process.env;
+dotenv.config();
+const { PORT } = process.env;
 
 app.listen(PORT, (request, response) => {
   console.log('Servidor corriendo en el puerto', PORT);
 });
 routerApi(app);
 
-mongoose.connect(URI_DB);
-const db = mongoose.connection;
-db.on('error', () =>  {
-  console.log('Error de conexion a la base de datos')
-});
-db.once('open', () => {
-  console.log('Connection a la base de datos exitosa');
-});
+
+
