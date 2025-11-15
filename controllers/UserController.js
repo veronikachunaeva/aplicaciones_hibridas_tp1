e6f1a0c5967cfc7c0b4e07100af266cdb8269390
nika-post <nika-post@yandex.ru>
2025-11-15 23:03:47 +0300

    Fix error

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
