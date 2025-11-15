7aa30cf610b0b7f9fd8e5b2697afbfa7d40507bc
nika-post <nika-post@yandex.ru>
2025-11-15 22:59:33 +0300

    Add imports

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,8 @@
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
