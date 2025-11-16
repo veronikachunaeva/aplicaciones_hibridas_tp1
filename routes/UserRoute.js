a7c9667640bc9a25b8bab91d19e201b8a53e65dc
nika-post <nika-post@yandex.ru>
2025-11-16 04:03:15 +0300

    Fix error

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,7 +3,7 @@ import { validateJWT} from '../middlewares/auth.js';
 import { createUser, getAllUsers, getUserById, updateUser, deleteUser, authUser } from '../controllers/UserController.js';    
 
 const router = express.Router();
-router.get('/', getAllUsers);
+router.get('/', validateJWT, getAllUsers);
 router.get('/:id', validateJWT, getUserById);
 router.post('/', createUser);
 router.put('/:id',validateJWT, updateUser);
