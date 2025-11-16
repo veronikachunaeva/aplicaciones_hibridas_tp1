3d5cde01ebb49cc55959a3058b7fca09f2261140
nika-post <nika-post@yandex.ru>
2025-11-16 03:51:47 +0300

    comment

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,7 +2,6 @@ import express from "express";
 import { validateJWT} from '../middlewares/auth.js';
 import { createUser, getAllUsers, getUserById, updateUser, deleteUser, authUser } from '../controllers/UserController.js';    
 
-
 const router = express.Router();
 router.get('/', getAllUsers);
 router.get('/:id', validateJWT, getUserById);
